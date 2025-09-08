5efe0bb0750fc7b063a34746f579c5b4f8c9cf4d
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2025-09-08 21:52:12 +0200

    Update Editor.tsx

--- packages/neuphlo-editor/src/react/Editor.tsx
+++ packages/neuphlo-editor/src/react/Editor.tsx
@@ -5,17 +5,19 @@ export type NeuphloEditorProps = {
   content?: string
   className?: string
   editable?: boolean
+  immediatelyRender?: boolean
 }
 
 export function Editor({
   content = "<p>Hello Neuphlo</p>",
   className,
   editable = true,
+  immediatelyRender = false,
 }: NeuphloEditorProps) {
   return (
     <div className={className}>
       <EditorProvider
-        immediatelyRender={false}
+        immediatelyRender={immediatelyRender}
         editable={editable}
         content={content}
         extensions={[StarterKit.configure({})]}
